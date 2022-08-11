edeafd49ab103f99a5a207a34429578ecb9adbb9
Jonysinger1 <jonysinger1@gmail.com>
2022-08-11 16:05:05 +0300

    save

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -6,7 +6,7 @@ import Navbar from "../components/Navbar";
 import Newsletter from "../components/Newsletter";
 import Products from "../components/Products";
 import Slider from "../components/Slider";
-
+//jony
 const Home = () => {
   return (
     <div>
